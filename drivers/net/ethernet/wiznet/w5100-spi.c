77bfc2a2fc51df3eebfc09c64f87802a7e64ffdf
Arthur Crepin-Leblond <arthur.crepin@proglove.de>
2023-10-04 10:24:40 +0200

    net:ethernet:wiznet:w5100-spi: Get the link gpio pin from the device tree

--- drivers/net/ethernet/wiznet/w5100-spi.c
+++ drivers/net/ethernet/wiznet/w5100-spi.c
@@ -16,6 +16,8 @@
 #include <linux/netdevice.h>
 #include <linux/of.h>
 #include <linux/of_net.h>
+#include <linux/of_device.h>
+#include <linux/of_gpio.h>
 #include <linux/spi/spi.h>
 
 #include "w5100.h"
@@ -423,6 +425,7 @@ static int w5100_spi_probe(struct spi_device *spi)
 	const struct w5100_ops *ops;
 	kernel_ulong_t driver_data;
 	const void *mac = NULL;
+	int link_gpio;
 	u8 tmpmac[ETH_ALEN];
 	int priv_size;
 	int ret;
@@ -450,7 +453,9 @@ static int w5100_spi_probe(struct spi_device *spi)
 		return -EINVAL;
 	}
 
-	return w5100_probe(&spi->dev, ops, priv_size, mac, spi->irq, -EINVAL);
+	link_gpio = of_get_named_gpio(spi->dev.of_node, "link-gpio", 0);
+
+	return w5100_probe(&spi->dev, ops, priv_size, mac, spi->irq, link_gpio);
 }
 
 static void w5100_spi_remove(struct spi_device *spi)
